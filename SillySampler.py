5b1fc33cdb22fcfa00adccf0598a37b541e1982a
Ghin_MLo7 <98134320+MLo7Ghinsan@users.noreply.github.com>
2025-07-23 18:37:38 -0500

    update version

--- SillySampler.py
+++ SillySampler.py
@@ -382,7 +382,7 @@ def run(server_class=ThreadedHTTPServer, handler_class=RequestHandler, port=8572
     print(f'Starting HTTP server on port {port}...')
     httpd.serve_forever()
 
-version = 'v1.0'
+version = 'v1.1'
 help_string = (
     'Usage:\n'
     '  SillySampler.py in.wav out.wav pitch velocity flags\n'
