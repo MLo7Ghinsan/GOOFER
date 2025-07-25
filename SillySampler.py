5f5ef20d62db73b985444f4f22a8375764b09e4d
Ghin_MLo7 <98134320+MLo7Ghinsan@users.noreply.github.com>
2025-07-24 19:18:58 -0500

    im gonna kms

--- SillySampler.py
+++ SillySampler.py
@@ -396,8 +396,12 @@ class GooferResampler:
                     lp_factor = 2 - abs_ten
                     hp_factor = abs_ten
                     if self.tension < 0:
-                        harmonic = dynamic_butter_filter(harmonic, f0_new, sr, lp_factor, order=4, btype='lowpass')
-                        aper_bre = dynamic_butter_filter(aper_bre, f0_new, sr, hp_factor, order=4, btype='highpass')
+                        abs_ten = abs(self.tension)
+                        order = int(np.round(1 + (abs_ten * 4)))
+                        order = np.clip(order, 1, 6)
+                        lp_factor = 2.0 - abs_ten * 1.0
+                        harmonic = dynamic_butter_filter(harmonic, f0_new, sr, lp_factor, order=order, btype='lowpass')
+                        aper_bre = dynamic_butter_filter(aper_bre, f0_new, sr, abs_ten, order=4, btype='highpass')
                     else:
                         highpassed = dynamic_butter_filter(harmonic, f0_new, sr, hp_factor * 4, order=4, btype='highpass')
                         boosted = highpassed * (1.0 + abs_ten * 20)
